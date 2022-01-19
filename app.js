5962324f71693d2a3607a1fd9815bd0358b359ba
Wonseok_Kong <suckiejjang@naver.com>
2022-01-19 22:48:09 +0900

    part 3.4-Events part Two

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
-const player = {
-    name: "albert",
-    hello: function () {
-        console.log("hello");
-    },
-};
+const title = document.querySelector("div.hello:first-child h1");
 
-console.log(player.name);
-player.hello("Kong");
+console.dir(title);
+
+function handleTitleClick() {
+    title.style.color = "blue";
+}
+
+function handleMouseEnter() {
+    // console.log("mouse is here!");
+    title.innerText = "mouse is here";
+}
+
+function handleMouseLeave() {
+    title.innerText = "mouse is gone!";
+}
+
+title.addEventListener("click", handleTitleClick);
+title.addEventListener("mouseenter", handleMouseEnter);
+title.addEventListener("mouseleave", handleMouseLeave);
