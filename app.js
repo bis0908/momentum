1c24977283ea44f71b41ddf8734598bcc5df951e
Wonseok_Kong <suckiejjang@naver.com>
2022-02-19 00:00:22 +0900

    2022-02-18 study

--- app.js
+++ app.js
@@ -1,12 +1,33 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const loginButton = document.querySelector("#login-form button");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+const ACTIONSUBMIT = "submit";
+
+const link = document.querySelector("a");
 
 function onLoginSubmit(event) {
-    event.preventDefault();
     // preventDefault: 어떤 event의 기본 행동을 수행하지 않도록 막아준다
-    // const username = loginInput.value;
-    console.log(loginInput);
+    event.preventDefault();
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    const username = loginInput.value;
+    localStorage.setItem(USERNAME_KEY, username);
+    paintGreetings(username);
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
+function paintGreetings(username) {
+    greeting.innerText = `HELLO! ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener(ACTIONSUBMIT, onLoginSubmit);
+} else {
+    paintGreetings();
+}
